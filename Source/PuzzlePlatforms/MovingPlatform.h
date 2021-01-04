7119bcfd31e3a82bac04d0f55666b5fe811d2199
MrHeadshtozz <rishabsaia@gmail.com>
2021-01-04 15:49:55 -0500

    Initial commit

---
+++ Source/PuzzlePlatforms/MovingPlatform.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "MovingPlatform.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEPLATFORMS_API AMovingPlatform : public AStaticMeshActor
+{
+	GENERATED_BODY()
+
+public:
+
+	AMovingPlatform();
+
+	void AddActiveTrigger();
+	void RemoveActiveTrigger();
+
+protected:
+
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere)
+	float Speed = 20;
+
+	UPROPERTY(EditAnywhere, Meta = (MakeEditWidget = true))
+	FVector TargetLocation;
+
+private:
+
+	FVector GlobalTargetLocation;
+
+	FVector GlobalStartLocation;
+
+	UPROPERTY(EditAnywhere)
+	int ActiveTriggers = 1;
+	
+};
